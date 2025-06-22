e51c23091e0b578fa0212aec2315a72d648d90f9
Mayank Agrawal <mayankagrawal.main@gmail.com>
2025-06-23 01:25:36 +0530

    Add tests for Point

---
+++ src/test/java/com/technologyconversations/kata/marsrover/PointTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2025 Smarsh Inc.
+ */
+
+package com.technologyconversations.kata.marsrover;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PointTest {
+    @Test
+    public void shouldIncreaseLocation() {
+        Point point = new Point(0, 8);
+        assertEquals(1, point.getForwardLocation());
+    }
+
+    @Test
+    public void shouldDecreaseLocation() {
+        Point point = new Point(4, 8);
+        assertEquals(3, point.getBackwardLocation());
+    }
+
+    @Test
+    public void shouldWrapAroundAtMaxLocation() {
+        Point point = new Point(8, 8);
+        assertEquals(0, point.getForwardLocation());
+    }
+
+    @Test
+    public void shouldWrapAroundAtZero() {
+        Point point = new Point(0, 8);
+        assertEquals(8, point.getBackwardLocation());
+    }
+}
